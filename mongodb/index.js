948fec40abc7bdc8cf01a634726ed506d2feb809
Nguyen <nguyen.nguyen942002@hcmut.edu.vn>
2023-04-03 16:09:47 +0700

    update mongodb/index.js

--- mongodb/index.js
+++ mongodb/index.js
@@ -1,25 +1,99 @@
-const express = require("express");
-const mongoose = require("mongoose");
+// Cài Mongoose: npm install --save mongoose
+// Cài ExpressJS: npm install express
 
+// Using ExpressJS (1)
+const express = require("express");
+const { Decimal128 } = require("mongodb");
+// Using ExpressJS (2)
 const app = express();
 
+// Bước tiếp theo, chúng ta sẽ tiến hành kết nối với MongoDB.
+const mongoose = require("mongoose");
 mongoose.connect(
   "mongodb+srv://nguyencharlie:smarthome2023@cluster0.ricdizx.mongodb.net/?retryWrites=true&w=majority",
   { useNewUrlParser: true }
 );
 
+// Chúng ta có một kết nối đang chờ xử lý đến cơ sở dữ liệu trên MongoDB Atlas (Server Singapore).
+// Bây giờ, cần bắt được sự kiện nếu chúng ta kết nối thành công hoặc nếu xảy ra lỗi kết nối:
 const db = mongoose.connection;
-
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
   console.log("Connected to database!");
 });
+const { Schema } = mongoose;
 
+// Tạo Schema:
 const fanSchema = new mongoose.Schema({
   _id: String,
-  timestamp: Date,
+  //General Attributes.
+  fanID: String,
   status: String,
+  //Private Attributes.
+  timestamp: Date,
   value: Number,
+});
+
+const bulbSchema = new mongoose.Schema({
+  _id: String,
+  //General Attributes.
+  bulbID: String,
+  status: String,
+  //Private Attributes.
+  timestamp: Date,
+  value: Number,
+});
+
+const tempSchema = new mongoose.Schema({
+  _id: String,
+  //General Attributes.
+  tempID: String,
+  timestamp: Date,
+  //Private Attributes.
+  temp: Decimal128,
+});
+
+const lightSchema = new mongoose.Schema({
+  _id: String,
+  //General Attributes.
+  lightID: String,
+  timestamp: Date,
+  //Private Attributes.
+  light: Decimal128,
+});
+
+const doorSchema = new mongoose.Schema({
+  _id: String,
+  //General Attributes.
+  doorID: String,
+  timestamp: Date,
+  //Private Attributes.
+  status: String,
+});
+
+const userSchema = new mongoose.Schema({
+  _id: String,
+  userID: String,
+  Fname: String,
+  Lname: String,
+  Email: String,
+  Password: String,
+  dateOfBirth: Date,
+  PhoneNumber: Number,
+});
+
+const userCommandSchema = new mongoose.Schema({});
+
+const homeSchema = new mongoose.Schema({
+  _id: String,
+  Key: Number,
+  HomeID: String,
+  Address: [{ HomeNumber: Number, Street: String, City: String }],
+});
+
+const roomSchema = new mongoose.Schema({
+  _id: String,
+  roomID: String,
 });
 
 const Fan = mongoose.model("Fan", fanSchema, "Fan");
